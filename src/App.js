4035e81f99e7c509c5248cf65a62594bdac57281
Dimas Neto <dimas_fcn@outlook.com>
2023-01-14 13:31:03 -0300

    form register

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import {
   Navigate,
   BrowserRouter,
 } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Compartilhe o seu conhecimento, entre para a comunidade!</h3>
+      <p>Blog React &copy; 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
