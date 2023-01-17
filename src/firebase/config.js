830b0217a18d4c5e013411db27c8d4524a870563
Dimas Neto <dimas_fcn@outlook.com>
2023-01-17 13:22:31 -0300

    useFetchDocuments

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,21 +15,3 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB1TYaDi-uulN1OQr8Mss4TXmAsyqCnDG8",
-//   authDomain: "blogreact-fc78b.firebaseapp.com",
-//   projectId: "blogreact-fc78b",
-//   storageBucket: "blogreact-fc78b.appspot.com",
-//   messagingSenderId: "185504056382",
-//   appId: "1:185504056382:web:b57b6a7046fb2a6453fd6f",
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// const db = getFirestore(app);
-
-// export { db };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,37 @@
 import styles from "./Home.module.css";
 
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Confira os últimos posts</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Buscar por tags"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button class="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h2>Posts...</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
